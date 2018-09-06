4f512523a9b868c1d147c8c1532c2db9ed825018
Opilane TTHK <opilane@tthk.ee>
2018-09-06 15:03:21 +0300

    Initial commit

---
+++ app/src/main/java/luhamaa/com/piibemann0609/MainActivity.java
@@ -0,0 +1,13 @@
+package luhamaa.com.piibemann0609;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
